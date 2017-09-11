166df2bf0b5d9b0a27a7c967a7fd1979e5cf65e7
root <root@localhost.localdomain>
2017-09-11 01:58:53 -0500

    Agregué los scripts extensions en /etc/asterisk

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+if [ "$( egrep -c 'regreso-ivr' /etc/asterisk/extensions_custom.conf)" != "2" ] ; then
+  cat ./extensions_custom.conf >> /etc/asterisk/extensions_custom.conf
+fi
+
+if [ "$( egrep -c 'regreso-ivr' /etc/asterisk/extensions_override_freepbx.conf)" != "1" ] ; then
+  cat ./extensions_override_freepbx.conf >> /etc/asterisk/extensions_override_freepbx.conf
+fi
+
 cp -fv ./regreso-ivr.py /var/lib/asterisk/agi-bin/regreso-ivr.py
 cp -fv ./regreso-ivr.wav /var/lib/asterisk/sounds/en/custom/regreso-ivr.wav
